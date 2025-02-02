3d24f2b05da6c1704d3ad22d9b31e0e79a987f58
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-02-02 13:52:03 +0200

    Add background images

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,10 +1,12 @@
 import AuthForm from "../../components/AuthForm/AuthForm";
+import css from "./SignupPage.module.css";
 
 const SignupPage = () => {
   return (
-    <>
+    <section className={css.background}>
       <AuthForm />
-    </>
+      <div className={css.bottle}></div>
+    </section>
   );
 };
 
